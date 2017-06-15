75013a493ccd5b1ed2d61119058462c1a464278a
Richard Pett <rich_pett@hotmail.com>
2017-06-15 19:12:43 +0100

    Changed Agencies to Agent

--- templates/agents/row-style2.php
+++ templates/agents/row-style2.php
@@ -34,7 +34,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 			            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 		            </h2>
 		            <div class="agencies">
-		            	Agencies
+		            	Agent
 		            </div>
 		            <div class="description">
 						<?php echo apustheme_substring( get_the_excerpt(), 18, '...' ); ?>
