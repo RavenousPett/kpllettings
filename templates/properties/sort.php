b8414a784dfbe0e8b1ec326195cc6bf2c62d8ae0
Richard Pett <rich_pett@hotmail.com>
2017-07-03 13:49:44 +0100

    Removed Order and Price/Title filters on property sort template. Leaving just to let / let by

--- templates/properties/sort.php
+++ templates/properties/sort.php
@@ -13,15 +13,6 @@
 
 		<div class="properties-sort-inner">
 
-
-			<div class="properties-filter-sort-by-wrapper">
-				<select class="form-control" name="filter-sort-by">
-					<option value=""><?php echo esc_html__( 'Sort by', 'preston' ); ?></option>
-					<option value="price" <?php if ( ! empty( $_GET['filter-sort-by'] ) && 'price' == $_GET['filter-sort-by'] ) :   ?>selected="selected"<?php endif; ?>><?php echo esc_html__( 'Price', 'preston' ); ?></option>
-					<option value="title" <?php if ( ! empty( $_GET['filter-sort-by'] ) && 'title' == $_GET['filter-sort-by'] ) :   ?>selected="selected"<?php endif; ?>><?php echo esc_html__( 'Title', 'preston' ); ?></option>
-				</select>
-			</div><!-- /.filter-sort-by-wrapper -->
-
             <div class="properties-filter-sort-by-wrapper">
                 <select class="form-control" name="filter-contract">
                     <option value=""><?php echo esc_html__( 'Contract', 'preston' ); ?></option>
@@ -31,13 +22,6 @@
                 </select>
             </div><!-- /.filter-sort-by-wrapper -->
 
-			<div class="properties-filter-sort-order-wrapper">
-				<select class="form-control" name="filter-sort-order">
-					<option value=""><?php echo esc_html__( 'Order', 'preston' ); ?></option>
-					<option value="asc" <?php if ( ! empty( $_GET['filter-sort-order'] ) && 'asc' == $_GET['filter-sort-order'] ) :   ?>selected="selected"<?php endif; ?>><?php echo esc_html__( 'ASC', 'preston' ); ?></option>
-					<option value="desc" <?php if ( ! empty( $_GET['filter-sort-order'] ) && 'desc' == $_GET['filter-sort-order'] ) :   ?>selected="selected"<?php endif; ?>><?php echo esc_html__( 'DESC', 'preston' ); ?></option>
-				</select>
-			</div><!-- /.filter-sort-order-wrapper-->
 		</div><!-- /.properties-sort-inner -->
 	</form>
 </div><!-- /.properties-sort -->
