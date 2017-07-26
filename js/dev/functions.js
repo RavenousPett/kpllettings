2338a0bf388f6fd7842022a9519906408760919e
Richard Pett <rich_pett@hotmail.com>
2017-07-26 19:46:19 +0100

    Code to hide the arrows until I pick this up again

--- js/dev/functions.js
+++ js/dev/functions.js
@@ -1079,7 +1079,7 @@ function makeArray( obj ) {
             $(".owl-carousel[data-carousel=owl]").each( function(){
                 var config = {
                     loop: false,
-                    nav: true,
+                    nav: false,
                     dots: $(this).data( 'pagination' ),
                     items: 4,
                     navText: ['<span class="fa fa-angle-left"></span>', '<span class="fa fa-angle-right"></span>']
